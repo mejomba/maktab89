3a129d90014883070bfc740619440e5e4393d8c0
mejomba <moojaminzadeh@gmail.com>
2022-12-29 17:04:51 +0330

    5

--- 5.py
+++ 5.py
@@ -7,7 +7,7 @@ out = "-".join(text[::-1])
 print(out)
 
 #
-text = '2014-12-27'
-match = re.search(r'(\d{4})-(\d{2})-(\d{2})', text)
+text = '2014-12/27'
+match = re.search(r'(\d{4})[-/](\d{2})[-/](\d{2})', text)
 result = re.sub(r'(\d{4})-(\d{2})-(\d{2})', f"{match.group(3)}-{match.group(2)}-{match.group(1)}", text)
 print(result)
