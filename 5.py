92436cab24531905f6f3658d4c8ab5dc7a7bc83c
mejomba <moojaminzadeh@gmail.com>
2022-12-29 15:42:42 +0330

    5

--- 5.py
+++ 5.py
@@ -5,3 +5,9 @@ pattern = r'-'
 text = re.split(pattern, text)
 out = "-".join(text[::-1])
 print(out)
+
+#
+text = '2014-12-27'
+match = re.search(r'(\d{4})-(\d{2})-(\d{2})', text)
+result = re.sub(r'(\d{4})-(\d{2})-(\d{2})', f"{match.group(3)}-{match.group(2)}-{match.group(1)}", text)
+print(result)
